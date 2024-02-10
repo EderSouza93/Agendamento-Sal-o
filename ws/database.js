a42ea3aea1d7876ca364e59057d7430a58b2beaa
Eder Souza <https://github.com/EderSouza93>
2024-02-10 11:55:42 -0300

    Conection with database (need fix error)

--- ws/database.js
+++ ws/database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const URI = '';
+const URI = 'mongodb+srv://salaoUser:zu0TwoBdn8Ne37jH@clusterdev.scywyeq.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', true);
+mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 mongoose.connect(URI)
 .then(() => console.log('DB is Up!'))
-.catch(() => console.log(err));
+.catch(() => console.log('Erro ao conectar ao DB'));
